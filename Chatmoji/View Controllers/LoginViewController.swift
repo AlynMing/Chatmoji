80238e35b3c856b010a998e3ff2357a731b2b39a
Andy Wang <andywang1197@gmail.com>
2020-11-07 14:57:47 -0800

    profileView implemented

--- Chatmoji/View Controllers/LoginViewController.swift
+++ Chatmoji/View Controllers/LoginViewController.swift
@@ -25,9 +25,12 @@ class LoginViewController: UIViewController, GIDSignInDelegate {
             if let error = error {
                 print(error.localizedDescription)
             } else {
-                print("Login Successful.")
-                //This is where you should add the functionality of successful login
-                //i.e. dismissing this view or push the home view controller etc    
+                print("Login Successful")
+                let profileView = self.storyboard?.instantiateViewController(withIdentifier: "ProfileViewController") as! ProfileViewController
+                self.present(profileView, animated: true, completion: nil)
+                
+
+                
             }
         }
     }
---
+++ Chatmoji/View Controllers/ProfileViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ProfileViewController.swift
+//  Chatmoji
+//
+//  Created by Andy Wang on 11/7/20.
+//  Copyright © 2020 andywang1197@icloud.com. All rights reserved.
+//
+
+import UIKit
+import GoogleSignIn
+import SDWebImage
+
+class ProfileViewController: UIViewController {
+
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let user: GIDGoogleUser = GIDSignIn.sharedInstance()!.currentUser
+        let fullName = user.profile.name
+        let email = user.profile.email
+        if user.profile.hasImage {
+        let userDP = user.profile.imageURL(withDimension: 200)
+        self.profileImageView.sd_setImage(with: userDP, placeholderImage: UIImage(named: "default-profile"))
+        } else {
+            self.profileImageView.image = UIImage(named: "default-profile")
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
